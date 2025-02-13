2038feeada9c01b62b6dee4b8fba75d10b6a4f65
Stephen Price <sprice@gpu-3-01.cm.cluster>
2025-02-13 14:50:09 -0500

    Expanded study

--- DualSight/ablationStudy/ablation_study.py
+++ DualSight/ablationStudy/ablation_study.py
@@ -355,7 +355,7 @@ if __name__ == "__main__":
 
     # Define experiment filters as desired
     experiment_filters = {
-        'ID': list(range(7180, 7280))
+        'ID': list(range(7480, 7480))
         # 'ID': [770]
         # 'ID': [3684]# + list(range(3611, 3684)) #+ list(range(3577,3599))
         # 'BoxInclusion': [True],
